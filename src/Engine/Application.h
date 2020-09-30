af5b127688df933bfdbc649fb2966edf7ae8baa9
Samuel Brown <samuelbrown3199@gmail.com>
2020-09-30 10:54:51 +0100

    Project setup

---
+++ src/Engine/Application.h
@@ -0,0 +1,7 @@
+
+class Application
+{
+public:
+
+	void Dummy();
+};
---
+++ src/TechDemo/main.cpp
@@ -0,0 +1,13 @@
+#include <Engine/Application.h>
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+
+	Application app;
+	app.Dummy();
+
+	return 0;
+}
